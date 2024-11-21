e13adc8a367beeccf9d55bf8ed4486aa673026fa
wellington-alme <168027822+wellington-alme@users.noreply.github.com>
2024-11-21 20:31:25 -0300

    Add files via upload

---
+++ prova web/index.js
@@ -0,0 +1,23 @@
+let valorpizzP = 25
+let valorpizzM = 35
+let valorpizzG = 50
+
+function valor_pizz(){
+    const pizzap = valorpizzP
+    const pizzam = valorpizzM
+    const pizzag = valorpizzG
+}
+
+let adicional = 5
+ function valor_adicionado(){  
+   const adc = adicional
+ }
+
+ let valortotal = 0;
+ console.log("Valor da sua comprar")
+
+
+
+
+
+ 
